c7721bd87908c2baadd1cd72df3dc16da50b74f5
Archit Khode <arkits@outlook.com>
2020-07-13 22:01:48 -0700

    implement gorrilla mux + GetBakchodByID endpoint

--- monitoring/api/controllers/bakchods.go
+++ monitoring/api/controllers/bakchods.go
@@ -2,15 +2,31 @@ package controllers
 
 import (
 	"encoding/json"
+	"log"
 	"net/http"
 
 	"github.com/arkits/chaddi-tg/monitoring/api/dao"
+	"github.com/gorilla/mux"
 )
 
-func BakchodsController(w http.ResponseWriter, r *http.Request) {
+func GetAllBakchodsController(w http.ResponseWriter, r *http.Request) {
 
 	allBakchods := dao.GetAllBakchods()
 
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(allBakchods)
+}
+
+func GetBakchodByIDController(w http.ResponseWriter, r *http.Request) {
+
+	vars := mux.Vars(r)
+
+	bakchodID := vars["bakchodID"]
+
+	log.Printf("GetBakchodByIDController bakchodID=%s", bakchodID)
+
+	bakchod := dao.GetBakchodByID(bakchodID)
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(bakchod)
 }
